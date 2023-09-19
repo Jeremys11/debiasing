f2411c98798465d56b6d255d61de91be127b61ae
Jeremys11 <jeremysingh116@gmail.com>
2023-09-19 15:57:16 -0400

    changed back to lstm

--- main.py
+++ main.py
@@ -9,12 +9,10 @@ import csv
 
 def main():
 
-    airnow_data, forecast_data = pickle.load(open("data/my_data_o3.p", "rb"))
+    airnow_data, forecast_data = pickle.load(open("data/my_data.p", "rb"))
 
     start_day = 24*0
     end_day = 24*2
-    #testing_locations = [250010002, 250051004, 250051006, 250070001, 250092006, 250095005, 250130008, 250154002, 250170009, 250213003, 250230005, 250250042, 250270024]
-    #my_location = 250010002
     testing_locations = [250010002]
 
     airnow_data = airnow_data.reset_index(drop=True)
@@ -22,7 +20,7 @@ def main():
 
     for my_location in testing_locations:
 
-        prediction_array = resnet_main(my_location)
+        prediction_array = lstm_main(my_location)
         
         temp_forecast = forecast_data["O3"][my_location,:].reset_index(drop=True)
 
@@ -31,7 +29,7 @@ def main():
         graph_prediction = prediction_array[start_day:end_day]
 
         pyplot.plot(graph_airnow, label='Airnow', color="black", linewidth=0.75)
-        pyplot.plot(graph_prediction, label='Resnet', color="orange", linewidth=0.75)
+        pyplot.plot(graph_prediction, label='LSTM', color="orange", linewidth=0.75)
         pyplot.plot(graph_forecast, label='Forecast', color="blue", linewidth=0.75)
         pyplot.xlabel("Forecast Hour")
         pyplot.ylabel("Ozone (ppb)")
@@ -47,9 +45,9 @@ def main():
 
         r_prediction, p_value1 = stats.pearsonr(graph_prediction,graph_airnow)
         r_forecast, p_value2 = stats.pearsonr(graph_forecast,graph_airnow)
-        print("airnow - Resnet Correlation: ", r_prediction)
+        print("airnow - LSTM Correlation: ", r_prediction)
         print("airnow - Forecast Correlation: ", r_forecast)
-        print("airnow - Resnet RMSE: ", airnow_vs_prediction)
+        print("airnow - LSTM RMSE: ", airnow_vs_prediction)
         print("airnow - forecast RMSE: ", airnow_vs_forecast)
 
         model_info = [
